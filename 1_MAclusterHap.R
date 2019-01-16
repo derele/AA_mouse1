4e90ea0ba5c7ad41b7d0a1580eb402a6affc1baf
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2019-01-16 11:04:42 +0100

    big leap after cleaning code

--- 1_MAclusterHap.R
+++ 1_MAclusterHap.R
@@ -4,6 +4,10 @@ library(data.table)
 library(DECIPHER)
 library(ape)
 library(pegas)
+library(sidier)
+library(adegenet)
+library(wordcloud)
+library(systemPipeR)
 
 FILTER <- TRUE
 newMA <- TRUE
@@ -30,7 +34,7 @@ names(filtRs) <- samples
 if (FILTER){
    filter.track <- lapply(seq_along(Ffq.file),  function (i) {
        filterAndTrim(Ffq.file[i], filtFs[i], Rfq.file[i], filtRs[i],
-                     truncLen=c(270,270), minLen=c(270,270), 
+                     truncLen=c(285,285), minLen=c(285,285), 
                      maxN=0, maxEE=2, truncQ=2, 
                      compress=TRUE, verbose=TRUE)
    })
@@ -69,21 +73,17 @@ if(newMA) {
         unlink(stratfiles,
                recursive=TRUE)
         }
-    MA1 <- sortAmplicons(MA, filedir= stratfiles
+    MA1 <- sortAmplicons(MA, filedir=stratfiles,
                          starting.at=1, max.mismatch=4)
     saveRDS(MA1, file="/SAN/Zebra/MA1.Rds")
+    pdf("figures/primers_MA_sorted.pdf", width=46)
+    plotAmpliconNumbers(MA1)
+    dev.off()
 } else {
     if(!newMA){
         MA1 <- readRDS(file="/SAN/Zebra/MA1.Rds")
     } else {stop("Whant new sorting or not? Set newMA to TRUE")}
 } 
-
-
-
-pdf("figures/primers_MA_sorted.pdf", width=46)
-plotAmpliconNumbers(MA1)
-dev.off()
-
 
 MA1 <- MA1[which(grepl("^Eim_Nuc", rownames(MA1))), ]
 
@@ -100,33 +100,29 @@ pdf("figures/primers_MA_EfalClade.pdf", width=45, height=15, onefile=FALSE)
 plotAmpliconNumbers(MA1)
 dev.off()
 
-## this only excludes one completely empty
-MA1 <- MA1[which(rowSums(getRawCounts(MA1))>2000), ]
-
-
 if(newDeDa){
     MA2 <- derepMulti(MA1, mc.cores=20)
     MA3 <- dadaMulti(MA2, Ferr=NULL, Rerr=NULL, selfConsist=TRUE,
                      multithread=FALSE, mc.cores=20, verbose=0, MAX_CONSIST=20)
-    MA4.merged <- mergeMulti(MA3, justConcatenate=FALSE, mc.cores=20)
-    prop.merged <- calcPropMerged(MA4.merged)
-    prop.merged[is.na(prop.merged)] <- 0
-    table(prop.merged>0.8)
-    MA4 <- mergeMulti(MA3, justConcatenate=prop.merged<0.8,
-                      mc.cores=20)
+    MA4 <- mergeMulti(MA3, mc.cores=20)
     MA5 <- makeSequenceTableMulti(MA4, mc.cores=20, orderBy="nsamples")
-    foo <- removeBimeraDenovo(getSequenceTable(MA5), multithread=TRUE, method="pooled")
-    MA6 <- MA5
-    MA6@sequenceTableNoChime <- foo
+    MA6 <- removeChimeraMulti(MA5)
     saveRDS(MA6, file="/SAN/Victors_playground/mouseGT/MA6.Rds")
 } else {
     MA6 <- readRDS(file="/SAN/Victors_playground/mouseGT/MA6.Rds")
 }
 
+
+## this only excludes one completely empty
+## subsetting is broken in package !!! Jan 5th.. 
+## MA6 <- MA6[which(rowSums(getRawCounts(MA6))>2000), ]
+
 STnoC <- getSequenceTableNoChime(MA6)
 
 ## discard empty samples and ASVs for each amplicon
 STnoC <- lapply(STnoC, function(x) x[rowSums(x)>0, colSums(x)>0])
+
+STnoC <- STnoC[unlist(lapply(STnoC, function (x) all(dim(x) >1)))]
 
 pdf("figures/all_amps_heated.pdf")
 lapply(STnoC, function (x) {
@@ -136,62 +132,37 @@ dev.off()
 
 
 ## get the most represented sequence for each amplicon and sample
-
-mostAbundant <- lapply(STnoC, function (amp){
-    apply(amp, 1, function (x){
+DNA <- lapply(STnoC, function (amp){
+    mostAbSeq <- apply(amp, 1, function (x){
         mostAbundant <- which(x==max(x))
-        list(names(x)[mostAbundant])
+        names(x)[mostAbundant]
     })
+    DNAStringSet(mostAbSeq)
 })
 
-### create empty sequences for each "null allele"
-mAdf <- as.data.frame(rbindlist(mostAbundant, fill=TRUE))
-ISnull <- sapply(mAdf, function (x) sapply(x, is.null))
-mAdf[ISnull] <- "NNN"
-
-#### Bring Ns for each null allel on the length of the sequence
-seqdf <- apply(mAdf, 1, function (x) {
-    slen <- max(nchar(x))
-    repl <- paste(rep("N", times=slen), collapse="")
-    gsub("NNN", repl, x)
-})
-
-DNA <- apply(seqdf, 2, function (x) { 
-    d <- DNAStringSet(as.character(x))
-    names(d) <- colnames(mAdf)
-    d
-})
-
-alnDNA1 <- lapply(DNA, AlignSeqs)
+## now we can remove the Ns and just let the alignment put gaps
+alnDNA1 <- lapply(DNA, function (x) AlignSeqs(x))
 
 ## now exclude too distant sequences (few seqs are from wrong targets
 ## I guess)
-distDNA <- lapply(alnDNA1, DistanceMatrix, penalizeGapLetterMatches = FALSE)
 
-## to see maximal distances
-lapply(distDNA, max)
-##  -> >20% difference is clearly nonsense
-
-## so we can exclude sequences which have with more than the are in
-## the minority as having huge differnces 
-differ <- lapply(distDNA, function (x) colSums(x>0.2))
-toExcl <- lapply(differ, function (x) which (x > length(x)/2)) 
-
-DNA2 <- lapply(seq_along(DNA), function (i) {
-    D <- DNA[[i]]
-    slen <- max(width(DNA[[i]]))
-    repl <- paste(rep("N", times=slen), collapse="")
-    D[toExcl[[i]]] <- repl
-    D
+DNA2 <- lapply(alnDNA1, function(x){
+    distDNA <- DistanceMatrix(x, penalizeGapLetterMatches = FALSE)
+    d <- colSums(distDNA>0.2, na.rm=TRUE)
+    ## so we can exclude sequences which have with more than the are in
+    ## the minority as having huge differnces 
+    toKeep <- which (d < (length(x)/2)) 
+    x[toKeep]
 })
 
-alnDNA2 <- lapply(DNA2, AlignSeqs)
+## realign removeing the gaps before
+alnDNA2 <- lapply(DNA2, function (x) AlignSeqs(DNAStringSet(gsub("-", "", x))))
 
 ## checking that huge distances are gone
 distDNA2 <- lapply(alnDNA2, DistanceMatrix, penalizeGapLetterMatches = FALSE)
 
 ## to see maximal distances
-lapply(distDNA2, max)
+summary(unlist(lapply(distDNA2, max, na.rm=TRUE)))
 
 ## to chech alignment length vs longest sequence
 longSeq <- unlist(lapply(DNA, function (x) max(width(x))))
@@ -199,70 +170,139 @@ alnSeq1 <- unlist(lapply(alnDNA1, function (x) max(width(x))))
 alnSeq2 <- unlist(lapply(alnDNA2, function (x) max(width(x))))
 cbind(longSeq, alnSeq1, alnSeq2)
 
+## remove the worst markers producing hardly any sequence for lots of
+## samples
+
+## remove duplicate technical replicate sequences for all samples
+## return NNN sequences to have all alignments have the same sequences
+
+all.reps <- unique(unlist(lapply(alnDNA2, names)))
+all.samples <- as.factor(gsub("-chip\\d", "", all.reps))
+
+Aln <- lapply(alnDNA2, function (aln){
+    alnWidth <- unique(width(aln))
+    sampleNames <- gsub("-chip\\d", "", names(aln))
+    ## could probably also work with an empty DNAstring("")
+    badString <- DNAString(paste(rep("N", times=alnWidth), collapse=""))
+    marker <- sapply(levels(all.samples), function (x){
+        pair <- which(sampleNames%in%x)
+        DNA <- aln[pair]
+        ## ## if it's only one sequence return it
+        if(length(DNA)<1){
+            return(badString)
+        }
+        if(all(!grepl("[A|G|C|T]", DNA))){return(badString)}
+        if(length(DNA)<2){return(DNA[[1]])}
+        ## ## if both are gap only return one of them (the first)        
+        ## PROBLEMATIC: What if the two technical reps don't agree?
+        ## repD <- DistanceMatrix(DNA)
+        ## if(any(repD>0)){
+        ##     warning(paste("replicate dist >0:", repD[repD>0][1]))
+        ##     return(DNAString(paste(rep("N", times=alnWidth),
+        ##                            collapse="")))
+        ## }
+        else {
+            return(DNA[[1]])
+        }
+    })
+    DNAStringSet(marker) 
+})
+
+## throw out markers with less then optimal number of sequences
+table(unlist(lapply(Aln, function (x) length(grep("[A|G|C|T]", x)))))
+
+Aln <- Aln[unlist(lapply(Aln, function (x) length(grep("[A|G|C|T]", x))))>13]
+
 ## to get the alingment partitions
-cumsum(alnSeq2)
+cumsum(unlist(lapply(Aln, function (x) max(width(x)))))
 
 ## paste the string sets togehter seq by seq
 pasteDNAstrings <- function (x, y){
     DNAStringSet(paste0(as.character(x), as.character(y)))
 }
 
-oneAlnDNA <- Reduce(pasteDNAstrings, alnDNA2)
-names(oneAlnDNA) <- colnames(mAdf)
+oneAln <- Reduce(pasteDNAstrings, Aln)
+>onames(oneAln) <- names(Aln[[1]])
 
-writeXStringSet(oneAlnDNA, file="/SAN/Victors_playground/mouseGT/combinedEfalNuc.fasta")
+writeXStringSet(oneAln, file="/SAN/Victors_playground/mouseGT/combinedEfalNuc.fasta")
 
-dbin <- as.DNAbin(oneAlnDNA)
+## create a Network representation despite missing data
 
-## library(sidier)
-## mutation.network(dbin)
-## mutation.network(dbinWO, network.method="zero")
+## dmat <- as.matrix(oneAln)
+dbin <- as.DNAbin(oneAln)
+gind <- DNAbin2genind(dbin)
 
-hapDNAString <- DNAStringSet(apply(as.character(hap), 1, paste, collapse=""))
+gind@type <- "PA"
 
-d <- DistanceMatrix(hapDNAString, penalizeGapLetterMatches = FALSE)
+gind@pop <- as.factor(ifelse(rownames(gind@tab)%in%c("36-SK2808", "37-SK2809"),
+                             "Mus", "Apodemus"))
 
-hap <- haplotype(dbin)
-hap <- sort(hap, what = "label")
+X <- tab(gind, NA.method="zero")
 
-pdf("figures/hapMDS.pdf")
-plotNetMDS(net, d)
+pca1 <- dudi.pca(X, scannf=FALSE, scale=FALSE)
+temp <- as.integer(pop(gind))
+myCol <- transp(c("blue","red"),.7)[temp]
+myPch <- c(15,17)[temp]
+
+
+pdf("figures/dudiPCA.pdf")
+plot(pca1$li, col=myCol, cex=3, pch=myPch)
+
+textplot(pca1$li[,1], pca1$li[,2], words=rownames(X), cex=1.4, new=FALSE)
+abline(h=0,v=0,col="grey",lty=2)
+
+## here it makes more senst to add up the contribution of different markers
+## s.arrow(pca1$c1*.5, add.plot=TRUE)
+
+legend("topright", pch=c(15,17), col=transp(c("blue","red"),.7),
+       leg=c("Apodemus","Mus"), pt.cex=2)
+
 dev.off()
 
-
-assoTab <- stack(setNames(attr(hap, "index"), rownames(hap)))
-
-assoTab$sample <- names(oneAlnDNA)[assoTab$values]
-
-ind.hap <- table(hap=assoTab$ind, individuals=assoTab$sample)
-
-net <- haploNet(hap, d)
-
-pdf("figures/hapNet.pdf", width=62, height=62)
-plot(net, size=attr(net, "freq"),
-     scale.ratio=0.2, pie=ind.hap)
-legend(-8, 0, colnames(ind.hap),
-       col=rainbow(nrow(assoTab)), pch=19, ncol=2)
-dev.off()
-
-## now with all gaps deleted
-## dbinWO <- del.colgapsonly(as.matrix(dbin), threshold = 0.01)
+## snpposi.plot(x = as.integer(seg.sites(dbin)),
+##              genome.size = length(dbin[[1]]))
 
 
-## hapWO <- haplotype(dbinWO)
-## hapWO <- sort(hapWO, what = "label")
+## Frame analysis:
+inFrameAln <- lapply(Aln, function (x) {
+    DNA <- list()
+    ## remove gaps and prefix G's which don't produce stop codons to
+    ## see what brings the alignment in frame
+    DNA[[1]] <- DNAStringSet(gsub("-|N", "", x))
+    DNA[[2]] <- DNAStringSet(paste0("G", DNA[[1]]))
+    DNA[[3]] <- DNAStringSet(paste0("GG", DNA[[1]]))
+    DNA[[4]] <- reverseComplement(DNAStringSet(gsub("-|N", "", x)))
+    DNA[[5]] <- reverseComplement(DNAStringSet(paste0(DNA[[1]], "C")))
+    DNA[[6]] <- reverseComplement(DNAStringSet(paste0(DNA[[1]], "CC")))
+    options(warn=-1)
+    ## max because we hope that one sequence at least hd no gaps at
+    ## all and we keep everthing at this length adding gaps back in
+    ## when aligning at the end
+    none <- max(nchar(gsub("\\*.*", "",
+                           translate(DNA[[1]][width(DNA[[1]])>0]))))
+    one <- max(nchar(gsub("\\*.*", "",
+                          translate(DNA[[2]][width(DNA[[2]])>1]))))
+    two <- max(nchar(gsub("\\*.*", "",
+                          translate(DNA[[3]][width(DNA[[3]])>2]))))
+    ## seems to be unnecessary, as all on + strand
+    noneR <- max(nchar(gsub("\\*.*", "",
+                           translate(DNA[[4]][width(DNA[[4]])>0]))))
+    oneR <- max(nchar(gsub("\\*.*", "",
+                          translate(DNA[[5]][width(DNA[[5]])>1]))))
+    twoR <- max(nchar(gsub("\\*.*", "",
+                           translate(DNA[[6]][width(DNA[[6]])>2]))))
+    options(warn=1)
+    ## hoping that one sequence has no gaps at all
+    orf.to <- max(c(none, one, two, noneR, oneR, twoR))
+    ## dodgy: having to avoid that in some cases (amplicon 30) tow
+    ## sequences have maximal orf length
+    this <- which(orf.to==max(orf.to))[[1]]
+    cat("THIS:", this, "\n")
+    list(AlignSeqs(DNA[[this]]), orf.to*3)
+})
 
-## netWO <- haploNet(hapWO)
+## yeah baby, now correct
+lapply(inFrameAln, function (x) cbind(unique(width(x[[1]])), x[[2]]))
 
-## assoTabWO <- stack(setNames(attr(hapWO, "index"), rownames(hapWO)))
-
-## assoTabWO$sample <- names(oneAlnDNA)[assoTabWO$values]
-
-## ind.hapWO <- table(hap=assoTabWO$ind, individuals=assoTabWO$sample)
-
-## pdf("figures/hapNetWO.pdf", width=62, height=62)
-## plot(netWO, size=attr(netWO, "freq"),
-##      scale.ratio=0.2, pie=ind.hapWO)
-## legend(-8, 0, colnames(ind.hapWO),
-##        col=rainbow(nrow(assoTabWO)), pch=19, ncol=2)
-## dev.off()
+frameAln <- lapply(inFrameAln, "[[", 1)
+toORF <- lapply(inFrameAln, "[[", 2)
