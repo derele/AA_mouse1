abe045d2d59895c1b8d555bec26bfc1c96942809
Alice <alice.balard@fu-berlin.de>
2019-04-09 20:31:30 +0200

    finish marker extraction for E64

---
+++ blast/blastE88.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+MYDB="/SAN/Eimeria_Wild_Genomes/Assemblies_Alice_final/E88_final.genome.scf.fasta"
+OUTFOLDER="/SAN/Alices_sandpit/AA_mouse1/git/AA_mouse1/blast/E88/"
+
+# do it once and for all
+makeblastdb -in $MYDB -dbtype nucl
+
+for i in /SAN/Alices_sandpit/AA_mouse1/data/seqApi/seq*fasta; 
+do echo `basename $i` ;
+blastn -query $i -db $MYDB -outfmt "10 qaccver saccver pident length mismatch gapopen qstart qend sstart send evalue bitscore staxid sseq qseq qframe sframe" -evalue 1e-5 -out $OUTFOLDER/`basename $i`.blastn; done
+
+for i in /SAN/Alices_sandpit/AA_mouse1/data/seqNucl/seq*fasta; 
+do echo `basename $i` ;
+blastn -query $i -db $MYDB -outfmt "10 qaccver saccver pident length mismatch gapopen qstart qend sstart send evalue bitscore staxid sseq qseq qframe sframe" -evalue 1e-5 -out $OUTFOLDER/`basename $i`.blastn; done
--- getFasta.R
+++ getFasta.R
@@ -3,154 +3,120 @@
 library(ShortRead)
 
 markers <- gsub(".fasta.blastn", "",
-                gsub("/SAN/Alices_sandpit/AA_EimeriaMarkers/blast/Efal/", "",
-                     list.files("/SAN/Alices_sandpit/AA_EimeriaMarkers/blast/Efal/")))
+                gsub("/SAN/Alices_sandpit/AA_mouse1/git/AA_mouse1/blast/Efal/", "",
+                     list.files("/SAN/Alices_sandpit/AA_mouse1/git/AA_mouse1/blast/Efal/")))
 
 markers.api <- markers[grep("Eim_Ap", markers)]
 markers.nucl <- markers[grep("Eim_Nuc", markers)]
 
-# Get the blast results:
-blastFullEfal <- data.frame()
-for (marker in markers){
-  blast <- read.csv(paste0("/SAN/Alices_sandpit/AA_EimeriaMarkers/blast/Efal/",
-                           marker, ".fasta.blastn"),
-                    header = F)
-  blast$marker <- marker
-  blastFullEfal <- rbind(blastFullEfal, blast)
-}
-
-blastFullEfal.Api <- blastFullEfal[grep("Eim_Ap", blastFullEfal$marker),]
-blastFullEfal.Nuc <- blastFullEfal[grep("Eim_Nuc", blastFullEfal$marker),]
-length(unique(blastFullEfal$marker)) # ok
-
-blastFullEver <- data.frame()
-for (marker in markers){
-    blast <- read.csv(paste0("/SAN/Alices_sandpit/AA_EimeriaMarkers/blast/Ever/",
-                           marker, ".fasta.blastn"),
-                    header = F)
-  blast$marker <- marker
-  blastFullEver <- rbind(blastFullEver, blast)
-}
-
-blastFullEver.Api <- blastFullEver[grep("Eim_Ap", blastFullEver$marker),]
-blastFullEver.Nuc <- blastFullEver[grep("Eim_Nuc", blastFullEver$marker),]
-length(unique(blastFullEver$marker)) # ok
-
-nameMyBlast <- function(blastdf, ref){
-  names(blastdf) <-  c("sample","saccver", "pident", "length", "mismatch", "gapopen", "qstart", "qend",
-                       "sstart", "send", "evalue", "bitscore", "staxid", ref, "queryseq", "qframe", "sframe", "marker")
-  return(blastdf)
-}
-
-blastFullEfal.Api <- nameMyBlast(blastFullEfal.Api, "Efalseq")
-blastFullEfal.Nuc <- nameMyBlast(blastFullEfal.Nuc, "Efalseq")
-blastFullEver.Api <- nameMyBlast(blastFullEver.Api, "Everseq")
-blastFullEver.Nuc <- nameMyBlast(blastFullEver.Nuc, "Everseq")
-
 # Get the raw sequences:
 dfFull.nucl <- data.frame()
 for (marker in markers.nucl){
-  df <- readFasta(paste0("/SAN/Alices_sandpit/AA_EimeriaMarkers/data/seqNucl/",
-                           marker, ".fasta"))
+  df <- readFasta(paste0("/SAN/Alices_sandpit/AA_mouse1/data/seqNucl/",
+                         marker, ".fasta"))
   df <- data.frame(FullSampleSeq = df@sread, marker = marker, sample = df@id)
   dfFull.nucl <- rbind(dfFull.nucl, df)
 }
 
 dfFull.api <- data.frame()
 for (marker in markers.api){
-  df <- readFasta(paste0("/SAN/Alices_sandpit/AA_EimeriaMarkers/data/seqApi/",
+  df <- readFasta(paste0("/SAN/Alices_sandpit/AA_mouse1/data/seqApi/",
                          marker, ".fasta"))
   df <- data.frame(FullSampleSeq = df@sread, marker = marker, sample = df@id)
   dfFull.api <- rbind(dfFull.api, df)
 }
 
-# merge both
-all.nuc.Efal <- merge(blastFullEfal.Nuc, dfFull.nucl)
-length(unique(all.nuc.Efal$marker)) # ok
-all.api.Efal <- merge(blastFullEfal.Api, dfFull.api)
-length(unique(all.api.Efal$marker)) # ok
-all.nuc.Ever <- merge(blastFullEver.Nuc, dfFull.nucl)
-length(unique(all.nuc.Ever$marker)) # ok
-all.api.Ever <- merge(blastFullEver.Api, dfFull.api)
-length(unique(all.api.Ever$marker)) # ok
+# Get the blast results:
+getBlastResults <- function(isolate){
+  blastFull <- data.frame()
+  for (marker in markers){
+    f <- paste0("/SAN/Alices_sandpit/AA_mouse1/git/AA_mouse1/blast/", isolate, "/",
+                marker, ".fasta.blastn")
+    if (file.size(f) > 0){
+      blast <- read.csv(f, header = F)
+      blast$marker <- marker
+      blastFull <- rbind(blastFull, blast)
+    }
+  }
+  nameMyBlast <- function(blastdf){
+    names(blastdf) <-  c("sample","saccver", "pident", "length", "mismatch", "gapopen", "qstart", "qend",
+                         "sstart", "send", "evalue", "bitscore", "staxid", isolate, "queryseq", "qframe", "sframe", "marker")
+    return(blastdf)
+  }
+  blastFull <- nameMyBlast(blastFull)
+  blastFull.Api <- blastFull[grep("Eim_Ap", blastFull$marker),]
+  blastFull.Nuc <- blastFull[grep("Eim_Nuc", blastFull$marker),]
+  return(list(blastFull = blastFull, blastFull.Api = blastFull.Api, blastFull.Nuc = blastFull.Nuc))
+}
+
+blastList_E139 <- getBlastResults(isolate = "E139")
+blastList_E88 <- getBlastResults(isolate = "E88")
+# errors for these 2, crapy genomes :( I need to go back there
+blastList_E64 <- getBlastResults(isolate = "E64")
+blastList_Efal <- getBlastResults(isolate = "Efal")
+blastList_Ever <- getBlastResults(isolate = "Ever")
+
+# Merge both
+
+# Efal
+all.Nuc.Efal <- merge(blastList_Efal$blastFull.Nuc, dfFull.nucl)
+length(unique(all.Nuc.Efal$marker)) # ok
+all.Api.Efal <- merge(blastList_Efal$blastFull.Api, dfFull.api)
+length(unique(all.Api.Efal$marker)) # ok
+
+# Ever
+all.Nuc.Ever <- merge(blastList_Ever$blastFull.Nuc, dfFull.nucl)
+length(unique(all.Nuc.Ever$marker)) # ok
+all.Api.Ever <- merge(blastList_Ever$blastFull.Api, dfFull.api)
+length(unique(all.Api.Ever$marker)) # ok
+
+# E64
+all.Nuc.E64 <- merge(blastList_E64$blastFull.Nuc, dfFull.nucl)
+length(unique(all.Nuc.E64$marker)) # only 20 were found by blast
+all.Api.E64 <- merge(blastList_E64$blastFull.Api, dfFull.api)
+length(unique(all.Api.E64$marker)) # ok
 
 # select the reference sequences to keep
-library(dplyr)
-
-final.nucl.Efal <- all.nuc.Efal %>%
-  distinct(Efalseq, marker, .keep_all = TRUE) %>%
-  group_by(marker) %>% # per marker
-  dplyr::filter(evalue == min(evalue)) %>% 
-  dplyr::filter(pident == max(pident)) %>% 
-  dplyr::filter(bitscore == max(bitscore)) 
-  
-final.api.Efal <- all.api.Efal %>%
-  distinct(Efalseq, marker, .keep_all = TRUE) %>%
-  group_by(marker) %>% # per marker
-  dplyr::filter(evalue == min(evalue)) %>% 
-  dplyr::filter(pident == max(pident)) %>% 
-  dplyr::filter(bitscore == max(bitscore)) 
-
-final.nucl.Ever <- all.nuc.Ever %>%
-  distinct(Everseq, marker, .keep_all = TRUE) %>%
-  group_by(marker) %>% # per marker
-  dplyr::filter(evalue == min(evalue)) %>% 
-  dplyr::filter(pident == max(pident)) %>% 
-  dplyr::filter(bitscore == max(bitscore)) 
-
-final.api.Ever <- all.api.Ever %>%
-  distinct(Everseq, marker, .keep_all = TRUE) %>%
-  group_by(marker) %>% # per marker
-  dplyr::filter(evalue == min(evalue)) %>% 
-  dplyr::filter(pident == max(pident)) %>% 
-  dplyr::filter(bitscore == max(bitscore)) 
-
 ## to put them in the order we need partition files
 # (also found for nucl in /SAN/Victors_playground/mouseGT/combinedEfalNucFramePartitions.csv)
 # incomplete for api, was done during extractMarkers.R by Alice
+fororderNuc <- read.csv("/SAN/Alices_sandpit/AA_mouse1/git/AA_mouse1/combinedEfalNucFramePartitions.csv")
+fororderApi <- read.csv("/SAN/Alices_sandpit/AA_mouse1/git/AA_mouse1/combinedEfalApiFramePartitions.csv")
 
-fororderNucl <- read.csv("/SAN/Alices_sandpit/AA_EimeriaMarkers/combinedEfalNucFramePartitions.csv")
-fororderApi <- read.csv("/SAN/Alices_sandpit/AA_EimeriaMarkers/combinedEfalApiFramePartitions.csv")
-
-names(fororderNucl)[1] <- "marker"
-fororderNucl$rank <- 1:nrow(fororderNucl)
-fororderNucl$marker <- as.character(fororderNucl$marker)
+names(fororderNuc)[1] <- "marker"
+fororderNuc$rank <- 1:nrow(fororderNuc)
+fororderNuc$marker <- as.character(fororderNuc$marker)
 names(fororderApi)[1] <- "marker"
 fororderApi$rank <- 1:nrow(fororderApi)
 fororderApi$marker <- as.character(fororderApi$marker)
 
-final.nucl.Efal$marker <- gsub("seq_", "", final.nucl.Efal$marker)
-final.nucl.Efal <- merge(final.nucl.Efal, fororderNucl)
-final.nucl.Efal <- final.nucl.Efal[order(final.nucl.Efal$rank), ]
-X <- paste0(">allMarkersNuclFromEfal\n", 
-            as.character(apply(final.nucl.Efal["Efalseq"], 2, function(row) paste(row, collapse = ""))))
-write.table(x = X, 
-            "/SAN/Alices_sandpit/AA_EimeriaMarkers/markersFromEfalNucl.fasta", 
-            row.names =  F, quote = F, col.names = F)
+library(dplyr)
 
-final.api.Efal$marker <- gsub("seq_", "", final.api.Efal$marker)
-final.api.Efal <- merge(final.api.Efal, fororderApi)
-final.api.Efal <- final.api.Efal[order(final.api.Efal$rank), ]
-X <- paste0(">allMarkersApiFromEfal\n", 
-            as.character(apply(final.api.Efal["Efalseq"], 2, function(row) paste(row, collapse = ""))))
-write.table(x = X, 
-            "/SAN/Alices_sandpit/AA_EimeriaMarkers/markersFromEfalApi.fasta", 
-            row.names =  F, quote = F, col.names = F)
+getRefSeq2Keep <- function(isolate, genome){
+  fororder = get(paste0("fororder", genome))
+  all = get(paste0("all.", genome, ".", isolate))
+  final = data.frame(all %>%
+                       distinct(marker, .keep_all = TRUE) %>%
+                       group_by(marker) %>% # per marker
+                       dplyr::filter(evalue == min(evalue)) %>% 
+                       dplyr::filter(pident == max(pident)) %>% 
+                       dplyr::filter(bitscore == max(bitscore)))
+  final$marker = gsub("seq_", "", final$marker)
+  final <- merge(final, fororder) # to order
+  final <- final[order(final$rank), ]
+  
+  longSeq = paste0(">", genome, isolate, "allMarkers\n",
+             as.character(apply(final[isolate], 2, function(row) paste(row, collapse = ""))))
+  write.table(x = longSeq,
+              paste0("/SAN/Alices_sandpit/AA_mouse1/git/AA_mouse1/markersByRef/", "markersFrom", isolate, ".", genome, ".fasta"),
+              row.names =  F, quote = F, col.names = F)
+  
+  return(list(final= final, longSeq = longSeq))
+}
 
-final.nucl.Ever$marker <- gsub("seq_", "", final.nucl.Ever$marker)
-final.nucl.Ever <- merge(final.nucl.Ever, fororderNucl)
-final.nucl.Ever <- final.nucl.Ever[order(final.nucl.Ever$rank), ]
-X <- paste0(">allMarkersNuclFromEver\n", 
-            as.character(apply(final.nucl.Ever["Everseq"], 2, function(row) paste(row, collapse = ""))))
-write.table(x = X, 
-            "/SAN/Alices_sandpit/AA_EimeriaMarkers/markersFromEverNucl.fasta", 
-            row.names =  F, quote = F, col.names = F)
-
-final.api.Ever$marker <- gsub("seq_", "", final.api.Ever$marker)
-final.api.Ever <- merge(final.api.Ever, fororderApi)
-final.api.Ever <- final.api.Ever[order(final.api.Ever$rank), ]
-X <- paste0(">allMarkersApiFromEver\n", 
-            as.character(apply(final.api.Ever["Everseq"], 2, function(row) paste(row, collapse = ""))))
-write.table(x = X, 
-            "/SAN/Alices_sandpit/AA_EimeriaMarkers/markersFromEverApi.fasta", 
-            row.names =  F, quote = F, col.names = F)
+final.nucl.Efal <- getRefSeq2Keep("Efal", "Nuc")
+final.api.Efal <- getRefSeq2Keep("Efal", "Api")
+final.nucl.Ever <- getRefSeq2Keep("Ever", "Nuc")
+final.api.Ever <- getRefSeq2Keep("Ever", "Api")
+final.nucl.E64 <- getRefSeq2Keep("E64", "Nuc")
+final.api.E64 <- getRefSeq2Keep("E64", "Api")
